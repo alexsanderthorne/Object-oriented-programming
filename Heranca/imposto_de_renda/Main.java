d4e4838af38ab8d0712eca5cc7c4259ef3aea212
Alexsander <alexsanderthorne@gmail.com>
2020-06-04 10:59:54 -0300

    sign up fp and jp

--- Heranca/imposto_de_renda/Main.java
+++ Heranca/imposto_de_renda/Main.java
@@ -9,14 +9,14 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
         String nome, cnpj, cpf;
-        double rendaBruta;
+        double rendaBruta = 0;
         // ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
         boolean check = true;
         int op = 0;
 
         while (check) {
 
-            System.out.println("*** *** ***");
+            System.out.println("*** *** =) ***");
             System.out.println("Calcular imposto de renda");
             System.out.println("*** *** ***");
             System.out.println("1- Cadastrar Pessoa Física");
@@ -24,27 +24,51 @@ public class Main {
             System.out.println("3- Calcular imposto de renda(procurar pelo cpf/cnpj)");
             System.out.println("0- Sair");
             System.out.println("*** *** ***");
+
             System.out.println("Digite a opção desejada : ");
+            op = scan.nextInt();
+            scan.nextLine();
 
             if (op == 3) {
                 int tipo_pessoa = 0;
+                System.out.println("Digite 1-Pessoa física. 2-Pessoa jurídica");
+                tipo_pessoa = scan.nextInt();
                 if (tipo_pessoa == 1) {
-                    System.out.println("Digite o nome");
-                    nome = scan.nextLine();
-                    System.out.println("Digite o cpf('ex:666.666.666-66')");
-                    cpf = scan.nextLine();
-                    System.out.println("Digite a renda bruta");
-                    rendaBruta = scan.nextDouble();
-                    // Pessoa p = new PessoaFisica(nome, rendaBruta, cpf);
-                    Pessoa p = new PessoaFisica(nome, rendaBruta, cpf);
+
+                    PessoaFisica p1 = new PessoaFisica();
+                    p1.setNome("joe cara de porco");
+                    p1.setRendaBruta(1000);
+                    p1.setCpf("0923r4fsdvged");
 
                     System.out.println("Pessoa física adicionada com sucesso!");
                     System.out.println("*** *** ***");
-                    System.out.println(nome + " Pagará de impostoR$ " + p.calcularImposto());
+                    System.out.println(p1.getNome() + " Pagará de impostoR$ " + p1.calcularImposto());
+
+                } else {
+
+                    System.out.println("Digite o nome");
+                    nome = scan.nextLine();
+                    System.out.println("digite a renda");
+                    rendaBruta = scan.nextDouble();
+                    System.out.println("Digite a porra do cnpj");
+                    cnpj = scan.nextLine();
+
+                    PessoaJuridica p1 = new PessoaJuridica(nome, rendaBruta, cnpj);
+
+                    System.out.println("Pessoa física adicionada com sucesso!");
+                    System.out.println("*** *** ***");
+                    System.out.println(p1.getNome() + " Pagará de impostoR$ " + p1.calcularImposto());
 
                 }
+            } else if (op == 0) {
+                check = false;
+
+            } else {
+                System.out.println("Opção inválida");
             }
         }
+
+        scan.close();
 
     }
 
