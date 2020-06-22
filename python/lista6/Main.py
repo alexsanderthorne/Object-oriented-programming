e33370d126598fcacda08192dafcdde5df24651b
dev <alexsanderthorne>
2020-06-21 23:16:11 -0300

    add read/write file, heritage

---
+++ python/lista6/Main.py
@@ -0,0 +1,31 @@
+#from Carro import Carro
+class Main(Carro):
+    op = 0
+    check = True
+
+    while(check):
+
+        print("1.Andar")
+        print("2.Reabastecer")
+        print("3.Verificar a quantidade de combustível")
+        print("4.Encerrar a viagem")
+
+        op = int(input("Digite a opção desejada : "))
+
+    if op == 1:
+
+        km = int(input("Digite a quilometragem : "))
+        c1 = Carro()
+        c1.carro.setEficiencia(20)
+        c1.setCombustivel(100)
+        c1.andar()
+        print("viagem...")
+
+    elif op == 2:
+
+        valor = int(input("Digite a qtd em litros : "))
+        Carro.Reabastecer(valor)
+
+    elif op == 0:
+        print("logout ...")
+        check = False
---
+++ python/write_file/arq.py
@@ -0,0 +1,7 @@
+filename = "newfile.txt"
+
+myfile = open(filename, 'a')
+
+myfile.write('write whith fucking python testing...')
+
+myfile.close()
